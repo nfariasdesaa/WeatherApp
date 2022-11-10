f156900c3ab5497db3148cdf9c20a1d63c3227c3
nfariasdesaa <nicolas.fariasds@gmail.com>
2022-11-10 13:04:24 -0300

    Initial commit

---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "c2cef044faedbacec361ec482bdfb598";
+    }
+}
